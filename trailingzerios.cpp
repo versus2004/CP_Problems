02a68b4c72a03f267d46faca1eba8e54d7d33f6f
versus2004 <shubhambind2004@gmail.com>
2024-01-29 22:10:06 +0530

    fixed

--- trailingzerios.cpp
+++ trailingzerios.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     long long m;
-    long count;
-    int rem=0;
-    cin>>m;
-        long long fact = 1;
-        while (m>0)
-        {
-            fact=fact*m;
-            m--;
-            /* code */
-        }
-        cout<<fact<<"\n";
-        while (fact%10==0)
-        {
-            count++;
-            fact=fact/10;
-        
-            /* code */
-        }
-        cout<<count;
-        
-        
+    long count = 0;
+    cin >> m;
+
+    long long fact = 1;
+
+    // Calculate factorial
+    while (m > 0) {
+        fact *= m;
+        m--;
+    }
+
+    cout <<  fact << "\n";
+
+    // Count the number of trailing zeros
+    while (fact % 10 == 0) {
+        count++;
+        fact /= 10;
+    }
+
+    cout << count << "\n";
+
+    return 0;
 }
