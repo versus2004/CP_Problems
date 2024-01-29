fe5d3ac1be3f9013acc09283b7e68fc6a65409bf
versus2004 <shubhambind2004@gmail.com>
2024-01-29 23:37:50 +0530

    edited

--- trailingzerios.cpp
+++ trailingzerios.cpp
@@ -1,27 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace mp = boost::multiprecision;
 using namespace std;
 
+mp::cpp_int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
 int main() {
-    long long m;
-    long count = 0;
+    int m;
+    cout << "Enter a number: ";
     cin >> m;
 
-    long long fact = 1;
-
-    // Calculate factorial
-    while (m > 0) {
-        fact *= m;
-        m--;
-    }
-
+    // Calculate factorial using Boost Multiprecision library
+    mp::cpp_int fact = factorial(m);
 
     // Count the number of trailing zeros
+    long count = 0;
     while (fact % 10 == 0) {
         count++;
         fact /= 10;
     }
 
-    cout << count << "\n";
+    cout << "Number of trailing zeros: " << count << "\n";
 
     return 0;
 }
